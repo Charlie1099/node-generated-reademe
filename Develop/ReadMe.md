ef65d2d82c821630bc7e2b4da798189308250200
charlie1099 <cdcharlesworth@gmail.com>
2021-10-03 20:24:58 -0600

    added to the mark down function

--- Develop/ReadMe.md
+++
@@ -1,5 +0,0 @@
-# awd
-  Description
- ddf
-  
-
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,52 +1,102 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(userResponses, data) {
- //generate tob if user selects it 
- let tableOfContents = `# Table of Contents`;
+  //generate tob if user selects it 
+  let tableOfContents = `# Table of Contents`;
 
- if (userResponses.installation !== "") {
-   tableOfContents += 
-   `* [Installation](#installation)`
- };
+  if (userResponses.installation !== "") {
+    tableOfContents +=
+      `* [Installation](#installation)`
+  };
 
- if (userResponses.usage !== "") {
-   tableOfContents +=
-  `* [Usage](#usage)`
-};
+  if (userResponses.usage !== "") {
+    tableOfContents +=
+      `* [Usage](#usage)`
+  };
 
-if (userResponses.contributing !== "") {
-  tableOfContents += 
-  `* [Contributing](#contributing)`
-};
+  if (userResponses.contributing !== "") {
+    tableOfContents +=
+      `* [Contributing](#contributing)`
+  };
 
-if (userResponses.test !== "") {
-  tableOfContents +=
-  `* [Test](#test)`
-};
+  if (userResponses.test !== "") {
+    tableOfContents +=
+      `* [Test](#test)`
+  };
+
+  let draftReadMe = `# ${userResponses.title}`
 
 
- 
-  return `# ${data.title}
+
+    `# ${data.title}
+
  # Description
  ${data.description}
- 
-draftM
+`
+  // add table of contents
+  draftReadMe += tableOfContents
 
- # Installatin
- ${data.installatin}
+  // license section 
+  draftReadMe += `
+*[license](#license)
+`
+  // optional install
+  if (userResponses.installation !== "") {
+    draftReadMe +=
+      `
+  
+  ## Tests
 
-`;
+  *Tests for application and how to run them:*
+
+  ${userResponses.test}
+
+  ## License
+
+  ${userResponses.license}
+  `
+    let draftDev =
+      `
+      ---
+
+      ##Questions?
+
+      ![Developer Profile Picture](${userInfo.avatar_url})
+
+      For any questions, please contact me at the information below:
+
+      GitHub: [@${userInfo.login}](${userInfo.url})
+  `;
+
+    if (userInfo.email !== null) {
+      draftDev +=
+        `
+    Email: ${userInfo.email}
+    `
+    };
+
+    //add developer section to markdown
+    draftReadMe += draftDev;
+
+    return draftReadMe;
+
+
+  };
+
+  return generateMarkdown
 }
 
 module.exports = generateMarkdown;
